667e4dce0d44b65754ab0ba61d642271e677276e
Adam Wenham <adamwenham64@gmail.com>
2013-05-27 16:48:02 +0100

    reached lvl 8

--- rohan-beginner/player.rb
+++ rohan-beginner/player.rb
@@ -1,13 +1,22 @@
 class Player
 
+  def initialize
+    @health = 20
+    @bounce = 0
+  end
+
   def play_turn(warrior)
     @warrior = warrior
     if danger_close?
       bang_him_out
-    elsif hurt? && !taking_damage?
+    elsif slightly_hurt? && !taking_damage?
       heal
     elsif captive_close?
       hero_time
+    elsif archer_issues?
+      retreat
+    elsif @warrior.feel.wall?
+        @warrior.pivot!
     else
       move_forward
     end
@@ -17,15 +26,23 @@ class Player
 ###################################################################################
 
   def danger_close?
-    @warrior.feel.enemy?
+    @warrior.feel.enemy? || @warrior.feel(:backward).enemy?
   end
 
   def bang_him_out
-    @warrior.attack!
+    if @warrior.feel.enemy?
+      @warrior.attack!
+    elsif @warrior.feel(:backward).enemy?
+      @warrior.attack!(:backward)
+    end
   end
 
-  def hurt?
+  def slightly_hurt?
     @warrior.health < 20
+  end
+
+  def badly_hurt?
+    @warrior.health < 12
   end
 
   def taking_damage?
@@ -40,11 +57,33 @@ class Player
     @warrior.walk!
   end
 
+  def move_backward
+    @warrior.walk!(:backward)
+  end
+
   def captive_close?
-    @warrior.feel.captive?
+    @warrior.feel.captive? || @warrior.feel(:backward).captive?
   end
 
   def hero_time
-    @warrior.rescue!
+    if @warrior.feel.captive?
+      @warrior.rescue!
+    elsif @warrior.feel(:backward).captive?
+      @warrior.rescue!(:backward)
+    end
+  end
+
+  def archer_issues?
+    taking_damage? && !danger_close? && badly_hurt?
+  end
+
+  def retreat
+    move_backward unless @warrior.feel(:backward).wall?
+  end
+
+  def bounce
+    if @warrior.feel(:backward).wall?
+      @bounce += 1
+    end
   end
 end
