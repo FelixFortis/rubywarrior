35bf84bc619e4a88a017a90f452807f5bf83956c
Adam Wenham <adamwenham64@gmail.com>
2013-05-27 14:45:15 +0100

    reached lvl 6

--- rohan-beginner/player.rb
+++ rohan-beginner/player.rb
@@ -1,5 +1,50 @@
 class Player
+
   def play_turn(warrior)
-    warrior.walk!
+    @warrior = warrior
+    if danger_close?
+      bang_him_out
+    elsif hurt? && !taking_damage?
+      heal
+    elsif captive_close?
+      hero_time
+    else
+      move_forward
+    end
+    @health = @warrior.health
+  end
+
+###################################################################################
+
+  def danger_close?
+    @warrior.feel.enemy?
+  end
+
+  def bang_him_out
+    @warrior.attack!
+  end
+
+  def hurt?
+    @warrior.health < 20
+  end
+
+  def taking_damage?
+    @warrior.health < @health
+  end
+
+  def heal
+    @warrior.rest!
+  end
+
+  def move_forward
+    @warrior.walk!
+  end
+
+  def captive_close?
+    @warrior.feel.captive?
+  end
+
+  def hero_time
+    @warrior.rescue!
   end
 end
