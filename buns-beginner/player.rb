e90036fd414262968e9c0dbb39182b517be4ab39
Adam Wenham <adamwenham64@gmail.com>
2013-07-31 07:15:07 +0100

    Completed level 4

--- buns-beginner/player.rb
+++ buns-beginner/player.rb
@@ -1,5 +1,28 @@
 class Player
+  def initialize
+    @health = 20
+  end
+
   def play_turn(warrior)
-    # add your code here
+    @buns = warrior
+
+    if !@buns.feel.empty?
+      @buns.attack!
+    else
+      if under_attack?
+        @buns.walk!
+      else
+        if @buns.health < 20
+          @buns.rest!
+        else
+          @buns.walk!
+        end
+      end
+    end
+    @health = @buns.health
+  end
+
+  def under_attack?
+    @health > @buns.health
   end
 end
